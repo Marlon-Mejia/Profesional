75c60ea62c762b5e07f279c399992868d4fd00eb
Mejia Marlon <mmejia@ab-sistemas>
2020-10-13 13:32:26 -0500

    Archivos

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Formulario :'v</title>
+</head>
+<body>
+    <form method="POST" action="suma.php">
+    <input type="text" name="num1" placeholder="Número 1">
+    <input type="text" name="num2" placeholder="Número 2">
+    <input type="submit" name="boton" value="Sumar">
+    </form>
+</body>
+</html>
---
+++ suma.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Suma</title>
+</head>
+<body>
+    <?php
+        $num1= $_POST['num1']
+        $num2= $_POST['num2']
+        $suma = $num1 + $num2
+        echo "El valor es: $suma"
+    ?>
+</body>
+</html>
+
+
